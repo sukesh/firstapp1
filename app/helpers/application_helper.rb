b5d432395ef7ac007692239f5a9b307d98dd7def
sukesh <sukesh@futurenow.biz>
2011-05-20 16:23:55 +0530

    added tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
 # Return a title on a per-page basis.
  def title
   base_title='Ruby on Rails Tutorial Sample App'
@@ -7,5 +8,8 @@ module ApplicationHelper
   else
    "#{base_title}|#{@title}"
    end
+  end
+  def logo
+  	image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
  end
